d0e634d1e156f1a35ca2ed3d4e16ddbe8b39f25a
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-08-28 20:23:56 +0530

    Finish action creator example

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import { useSelector } from 'react-redux';
-
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { sendCartData } from "./store/cart-slice";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
-
+  const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  const notification = useSelector((state) => state.ui.notification);
+  useEffect(() => {
+    dispatch(sendCartData(cart));
+  }, [cart, dispatch]);
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      <p>{notification?.status}</p>
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,7 +1,7 @@
-import { createSlice } from '@reduxjs/toolkit';
-
+import { createSlice } from "@reduxjs/toolkit";
+import { uiActions } from "./ui-slice";
 const cartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState: {
     items: [],
     totalQuantity: 0,
@@ -40,6 +40,51 @@ const cartSlice = createSlice({
     },
   },
 });
+
+export const sendCartData = (cartData) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "Pending..",
+        title: "Sending...",
+        message: "Sending cart data!",
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://redux-sideeffects-d55c9-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cartData),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+    };
+
+    try {
+      await sendRequest();
+      dispatch(
+        uiActions.showNotification({
+          status: "Successfull!",
+          title: "Sent cart data!",
+          message: "Sent cart successfully!",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "Error",
+          title: "Error",
+          message: "Something went wrong!",
+        })
+      );
+    }
+  };
+};
 
 export const cartActions = cartSlice.actions;
 
